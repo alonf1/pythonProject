0bb9297319b4066f825eb886c9e0bc97e28e887a
alonf1 <Alonfalah@gmail.com>
2023-07-21 20:57:43 +0300

    init commit

--- Python5.py
+++ Python5.py
@@ -1,5 +1,4 @@
 my_items = [{"name": "aviel", "age": 33}, {"name": "moshe", "age": 20},
-            {"name": "david", "age": 60}]
+            {"name": "david", "age": 50}]
 aa = [item["name"] for item in my_items if item["age"] > 30]
 print(aa)
-aaa = "bbb"
